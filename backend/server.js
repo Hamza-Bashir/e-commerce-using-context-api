10f1dbd73b590cc1d9d77dd6741af819854c1980
Hamza <hmza@gmail.com>
2025-03-06 16:03:13 +0500

    Add cors in server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const db = require("./db/conn")
+const cors = require("cors")
 require("dotenv").config()
 const userRouter = require("./routes/userRouter")
+
 
 const app = express()
 
 const port = process.env.PORT
 
+app.use(cors())
+
+
 app.use(express.json())
 app.use("/", userRouter)
+
+
 
 
 app.listen(port, ()=>{
